d9ee083c3720f63a51a3a0b91fab99aa51563088
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-06-22 23:44:20 -0300

    implementation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,14 +15,53 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const reducer = (accumulator: number, currentValue: number) =>
+      accumulator + currentValue;
+    const incomeValue =
+      this.transactions.filter(transation => transation.type === 'income')
+        .length === 0
+        ? 0
+        : this.transactions
+            .filter(transation => transation.type === 'income')
+            .map(transation => {
+              return transation.value;
+            })
+            .reduce(reducer);
+
+    const outcomeValue =
+      this.transactions.filter(transation => transation.type === 'outcome')
+        .length === 0
+        ? 0
+        : this.transactions
+            .filter(transation => transation.type === 'outcome')
+            .map(transation => {
+              return transation.value;
+            })
+            .reduce(reducer);
+
+    return {
+      income: incomeValue,
+      outcome: outcomeValue,
+      total: incomeValue - outcomeValue,
+    };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
     // TODO
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
